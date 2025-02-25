53093d56461d8e9bec31a8c27966184e70083ff6
Pradeep <pawarpradeep1707@gmail.com>
2025-02-25 16:10:50 +0530

    Added a smoothpageindicator

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PageController _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
+      body: Stack(
         children: [
-          Container(color: Colors.blue),
-          Container(color: Colors.yellow),
-          Container(color: Colors.green),
+          PageView(
+            children: [
+              Container(color: Colors.blue),
+              Container(color: Colors.yellow),
+              Container(color: Colors.green),
+            ],
+          ),
+          Container(
+              alignment: Alignment(0, 0),
+              child: SmoothPageIndicator(controller: _pageController, count: 3))
         ],
       ),
     );
